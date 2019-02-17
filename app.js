5a5210dfc5e999f1c4142959165e14b222fdb928
kimsejune <tpwns1088@gmail.com>
2019-02-17 19:39:47 +0900

    isSpam Function 구현
    isSpam Function 구현

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
-
 const app = express();
 
+
 app.get('/', (req, res) => {
-  res.send('Root Router');
+  const content = 'spam spam https://goo.gl/nVLutc';
+  const spamLinkDomains = 'www.filekok.com';
+  const redirectionDepth = 3;
+  const flag = isSpam(content, spamLinkDomains, redirectionDepth);
+  console.log(flag);
+  res.send(flag)
 });
+
+function isSpam(content, spamLinkDomains, redirectionDepth) {
+
+  const url = content.split(' ');
+  const spamRedirectUrl = ['bit.ly', 'tvtv24.com', 'www.filekok.com'];
+  if(spamLinkDomains == spamRedirectUrl[redirectionDepth-1]) {
+    return true;
+  }
+  return false;
+}
 
 
 app.listen('3000', () => {
